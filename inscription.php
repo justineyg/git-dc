d68c13b9d44512d209d2435f1bfa7e178ed49e59
justineyg <justineyang99@gmail.com>
2021-10-21 12:04:24 +0200

    modification inscription

--- inscription.php
+++ inscription.php
@@ -6,6 +6,52 @@
 </head>
 <body>
     <?php include('partials/menu.php'); ?>
-    <h1>Page d'inscription gros bg</h1>
+    <h1>Page d'inscription Employés</h1>
+
+    <form action="POST">
+
+    <p>
+		<label for="nom">Nom</label>
+		<input type="text" name="nom" id="nom">
+	</p>
+
+	<p>
+		<label for="prenom">Prénom</label>
+		<input type="text" name="duree" id="prenom">
+	</p>
+
+	<p>
+		Sexe : 
+
+
+		<input type="radio" name="sexe" id="m" value="m">
+		<label for="m">m</label>
+
+
+		<input type="radio" name="sexe" id="f" value="f">
+		<label for="f">f</label>
+
+
+
+	<p>
+		<label for="service">Service</label>
+		<input name="text" id="service"></input>
+	</p>
+
+	<p>
+		<label for="date_embauche">Date d'embauche </label>
+		<input type="date" name="date_embauche" placeholder="La date d'embauche' au format YYYY-MM-JJ" id="date_embauche">
+	</p>
+
+    <p>
+		<label for="salaire">Salaire</label>
+		<input type="text" name="salaire" id="salaire">
+	</p>
+
+    <p>
+		<button type="submit">Ajouter</button>
+	</p>
+
+    </form>
 </body>
 </html>
