54d39678da51aa21682b11d258a2b788fb73a941
Shiro <shiro@MacBook-Pro-Shiro.local>
2021-10-21 11:35:40 +0200

    Ajout fichier CSS, arborescence plus propre

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include('partials/head.php'); ?>
     <title>Site Employés</title>
 </head>
 <body>
-    <?php include('menu.php'); ?>
+    <?php include('partials/menu.php'); ?>
     <h1>Test bonjour hahaha</h1>
 </body>
 </html>
