f18c4bc0a6fbefd55d1b8aa37ead55314fe0344a
justineyg <justineyang99@gmail.com>
2021-10-21 10:39:36 +0200

    bdd

---
+++ bdd.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le : jeu. 21 oct. 2021 à 08:37
+-- Version du serveur :  5.7.30
+-- Version de PHP : 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de données : `entreprise`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `employes`
+--
+
+CREATE TABLE `employes` (
+  `id_employes` int(11) NOT NULL,
+  `prenom` varchar(20) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `sexe` enum('m','f') NOT NULL,
+  `service` varchar(30) NOT NULL,
+  `date_embauche` date NOT NULL,
+  `salaire` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `employes`
+--
+
+INSERT INTO `employes` (`id_employes`, `prenom`, `nom`, `sexe`, `service`, `date_embauche`, `salaire`) VALUES
+(350, 'Jean-pierre', 'Laborde', 'm', 'direction', '2010-12-09', 5000),
+(388, 'Clement', 'Gallet', 'm', 'commercial', '2010-12-15', 2300),
+(415, 'Thomas', 'Winter', 'm', 'commercial', '2011-05-03', 3550),
+(417, 'Chloe', 'Dubar', 'f', 'production', '2011-09-05', 1900),
+(491, 'Elodie', 'Fellier', 'f', 'secretariat', '2011-11-22', 1600),
+(509, 'Fabrice', 'Grand', 'm', 'comptabilite', '2011-12-30', 2900),
+(547, 'Melanie', 'Collier', 'f', 'commercial', '2012-01-08', 3100),
+(592, 'Laura', 'Blanchet', 'f', 'direction', '2012-05-09', 4500),
+(627, 'Guillaume', 'Miller', 'm', 'commercial', '2012-07-02', 1900),
+(655, 'Celine', 'Perrin', 'f', 'commercial', '2012-09-10', 2700),
+(699, 'Julien', 'Cottet', 'm', 'secretariat', '2013-01-05', 1390),
+(701, 'Mathieu', 'Vignal', 'm', 'informatique', '2013-04-03', 2500),
+(739, 'Thierry', 'Desprez', 'm', 'secretariat', '2013-07-17', 1500),
+(780, 'Amandine', 'Thoyer', 'f', 'communication', '2014-01-23', 2100),
+(802, 'Damien', 'Durand', 'm', 'informatique', '2014-07-05', 2250),
+(854, 'Daniel', 'Chevel', 'm', 'informatique', '2015-09-28', 3100),
+(876, 'Nathalie', 'Martin', 'f', 'juridique', '2016-01-12', 3550),
+(900, 'Benoit', 'Lagarde', 'm', 'production', '2016-06-03', 2550),
+(933, 'Emilie', 'Sennard', 'f', 'commercial', '2017-01-11', 1800),
+(990, 'Stephanie', 'Lafaye', 'f', 'assistant', '2017-03-01', 1775);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `employes`
+--
+ALTER TABLE `employes`
+  ADD PRIMARY KEY (`id_employes`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `employes`
+--
+ALTER TABLE `employes`
+  MODIFY `id_employes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=991;
